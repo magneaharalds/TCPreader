a383f119bf3d2c17ce212efe6f8b71f5471a86a8
magneaharalds <haraldsdottir.magnea@gmail.com>
2022-06-13 22:59:56 +0200

    Sort and printing approx every 10 sec

--- src/TcpReader.java
+++ src/TcpReader.java
@@ -12,17 +12,16 @@ public class TcpReader {
         Hashtable<String, Integer> ip_addresses = new Hashtable<String, Integer>();
         int ip_send_ind = 2;
         int ip_recieve_ind = 4;
-
+        long time = System.currentTimeMillis();
         while (scan.hasNextLine()) {
             String line = scan.nextLine();
             String[] elems = line.split(" ");
-            //System.out.println(elems[ip_send_ind]);
-            //System.out.println(elems[ip_recieve_ind]);
-            // check if action was sending bytes
-            //System.out.println(elems[elems.length-2]);
-            //System.out.println(elems[ip_send_ind].lastIndexOf("."));
 
-            if(elems[elems.length-2].equals("length")){
+            // check if action was sending bytes
+            if(elems[elems.length-2].equals("length") && elems[1].equals("IP")){
+                /*
+                * Problems with IP addresses with no port number
+                 */
                 String ip_send = elems[ip_send_ind].substring(0 , elems[ip_send_ind].lastIndexOf("."));
                 String ip_recieve = elems[ip_recieve_ind].substring(0 , elems[ip_recieve_ind].lastIndexOf("."));
 
@@ -47,18 +46,39 @@ public class TcpReader {
                     ip_addresses.put(ip_recieve, 1);
                 }
             }
-            System.out.println(ip_addresses);
-            System.out.println(bytes_out);
-            System.out.println(bytes_in);
+            //sortValue(ip_addresses);
+
+            if (time+10000 < System.currentTimeMillis()){
+                time = System.currentTimeMillis();
+                System.out.println("Sum of ingoing traffic in bytes: " + bytes_in);
+                System.out.println("Sum of outgoing traffic in bytes: " + bytes_out);
+                ArrayList<Map.Entry<String, Integer>> sorted = sortValue(ip_addresses);
+                System.out.println("Most frequently seen ip-addresses:");
+                if(sorted.size()>10){
+                    for(int i = 0; i <10; i++){
+                        System.out.println(sorted.get(i));
+                    }
+                }else{
+                    for(int i = 0; i < sorted.size();i++){
+                        System.out.println(sorted.get(i));
+                    }
+
+                }
+                System.out.println();
+            }
         }
     }
 
-    /*while stdin
-    lesa streng þangað til \n
-    Splitta streng á bilum
-    Finna 192.168
-    Finna ekki 192 168
-    Finna það sem kemur eftir length
-    Skila öllu þessu í 3x fylki*/
+    public static ArrayList<Map.Entry<String, Integer>> sortValue(Hashtable<String, Integer> t){
 
+        //Transfer as List and sort it
+        ArrayList<Map.Entry<String, Integer>> l = new ArrayList(t.entrySet());
+        Collections.sort(l, new Comparator<Map.Entry<String, Integer>>(){
+
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                return o2.getValue().compareTo(o1.getValue());
+            }});
+
+        return l;
+    }
 }
